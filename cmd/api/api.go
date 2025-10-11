feb5f86b7a449f6780d34ac8dd64afa5ae94a42d
Mohamed Ibranim <mosuliman92@gmail.com>
2025-10-11 18:27:59 +0300

    call db initialization

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,16 +5,26 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/MohummedSoliman/social/internal/store"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
 	config config
+	store  store.Storage
+	db     dbConfig
 }
 
 type config struct {
 	addr string
+}
+
+type dbConfig struct {
+	addr         string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
 }
 
 func (app *application) mount() http.Handler {
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,14 +3,34 @@ package main
 import (
 	"log"
 
+	"github.com/MohummedSoliman/social/internal/db"
 	"github.com/MohummedSoliman/social/internal/env"
+	"github.com/MohummedSoliman/social/internal/store"
 )
 
 func main() {
+	cfg := dbConfig{
+		addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+		maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+		maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+		maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+	}
+
+	db, err := db.New(cfg.addr, cfg.maxOpenConns, cfg.maxIdleConns, cfg.maxIdleTime)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+	log.Println("postgres connection pool establish sucessfully!!")
+
+	store := store.NewStorage(db)
+
 	app := &application{
 		config: config{
 			addr: env.GetString("ADDR", ":8080"),
 		},
+		db:    cfg,
+		store: store,
 	}
 
 	mux := app.mount()
