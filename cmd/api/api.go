9f81d27eb8af8edc17326b5a176ae1f9d66d0683
Mohamed Ibranim <mosuliman92@gmail.com>
2025-10-11 11:37:47 +0300

    add the project boilerpate

---
+++ cmd/api/api.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) mount() http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.RealIP)
+	mux.Use(middleware.RequestID)
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.Logger)
+
+	mux.Use(middleware.Timeout(60 * time.Second))
+
+	mux.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+
+	return mux
+}
+
+func (app *application) run(mux http.Handler) error {
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Println("Server has started at: ", app.config.addr)
+
+	return srv.ListenAndServe()
+}
