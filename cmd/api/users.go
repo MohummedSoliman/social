363c30f70809d5ea81dadeb9cf0661b8e2911eda
Mohamed Ibranim <mosuliman92@gmail.com>
2025-10-15 09:31:52 +0300

    add two handlers for hadndling new two routes

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"net/http"
 	"strconv"
@@ -9,27 +10,93 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
-	idParam := chi.URLParam(r, "userID")
-	userID, err := strconv.Atoi(idParam)
-	if err != nil {
-		app.badRequest(w, r, err)
-		return
-	}
+type userContextKeys string
 
-	user, err := app.store.Users.GetUserByID(r.Context(), int64(userID))
-	if err != nil {
-		switch {
-		case errors.Is(err, store.ErrNotFound):
-			app.badRequest(w, r, err)
-		default:
-			app.internalServerError(w, r, err)
-		}
-		return
-	}
+var USERKEY userContextKeys = "user"
+
+func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := getUserFromContext(r)
 
 	if err := jsonResponse(w, http.StatusOK, user); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
+}
+
+type FollowUser struct {
+	UserID int64 `json:"user_id"`
+}
+
+func (app *application) followUserHandler(w http.ResponseWriter, r *http.Request) {
+	followerUser := getUserFromContext(r)
+
+	var payload FollowUser
+	err := readJSON(w, r, &payload)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+
+	err = app.store.Followers.Follow(r.Context(), followerUser.ID, payload.UserID)
+	if err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	if err := jsonResponse(w, http.StatusNoContent, nil); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}
+
+func (app *application) unFollowUserHandler(w http.ResponseWriter, r *http.Request) {
+	unfollowedUser := getUserFromContext(r)
+
+	var payload FollowUser
+	err := readJSON(w, r, &payload)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+
+	err = app.store.Followers.UnFollow(r.Context(), unfollowedUser.ID, payload.UserID)
+	if err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	if err := jsonResponse(w, http.StatusNoContent, nil); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+}
+
+func (app *application) userContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		idParam := chi.URLParam(r, "userID")
+		userID, err := strconv.Atoi(idParam)
+		if err != nil {
+			app.badRequest(w, r, err)
+			return
+		}
+
+		user, err := app.store.Users.GetUserByID(r.Context(), int64(userID))
+		if err != nil {
+			switch {
+			case errors.Is(err, store.ErrNotFound):
+				app.badRequest(w, r, err)
+			default:
+				app.internalServerError(w, r, err)
+			}
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), USERKEY, user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func getUserFromContext(r *http.Request) *store.User {
+	user := r.Context().Value(USERKEY).(*store.User)
+	return user
 }
