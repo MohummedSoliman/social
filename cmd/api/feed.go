ea9f340a5490ac7e31b7699454809f875a6e3691
Mohamed Ibranim <mosuliman92@gmail.com>
2025-10-15 17:15:06 +0300

    add code that call feedpagination parse method and then vallidate it

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -1,11 +1,33 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/MohummedSoliman/social/internal/store"
+)
 
 func (app *application) getUserFeedhandler(w http.ResponseWriter, r *http.Request) {
+	fq := store.PaginatedFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+	}
+
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+
+	err = Validate.Struct(fq)
+	if err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
 	ctx := r.Context()
 
-	feeds, err := app.store.Posts.GetUserFeed(ctx, int64(1))
+	feeds, err := app.store.Posts.GetUserFeed(ctx, int64(1), fq)
 	if err != nil {
 		app.internalServerError(w, r, err)
 		return
