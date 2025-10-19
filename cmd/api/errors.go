c6750ba3205afb7e01e60763e39532b446c508e4
Mohamed Ibranim <mosuliman92@gmail.com>
2025-10-19 13:37:31 +0300

    add errors related to basic authentication method

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -19,3 +19,14 @@ func (app *application) notFoundError(w http.ResponseWriter, r *http.Request, er
 	log.Printf("not found error : %s path: %s error: %s", r.Method, r.URL.Path, err)
 	writeJSONError(w, http.StatusNotFound, "Bad Request Data")
 }
+
+func (app *application) unauthorizedError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("UnAuthorized Error : %s path: %s error: %s", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusUnauthorized, "unauthorized")
+}
+
+func (app *application) unauthorizedBasicError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("UnAuthorized Basic Error :  %s path: %s error: %s", r.Method, r.URL.Path, err)
+	w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+	writeJSONError(w, http.StatusUnauthorized, "unauthorized")
+}
