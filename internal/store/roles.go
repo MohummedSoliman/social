0c3461fd01c01e214a4d8543253d1753008e0efe
Mohamed Ibranim <mosuliman92@gmail.com>
2025-10-20 10:55:36 +0300

    add role struct and db methods relate to it

---
+++ internal/store/roles.go
@@ -0,0 +1,43 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Role struct {
+	ID          int64  `json:"id"`
+	Name        string `json:"name"`
+	Level       int    `json:"level"`
+	Description string `json:"description"`
+}
+
+type RoleStore struct {
+	db *sql.DB
+}
+
+func (r *RoleStore) GetByName(ctx context.Context, roleName string) (*Role, error) {
+	query := `SELECT id, name, level, description FROM roles WHERE name = $1`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	var role Role
+	row := r.db.QueryRowContext(ctx, query, roleName)
+	err := row.Scan(
+		&role.ID,
+		&role.Name,
+		&role.Level,
+		&role.Description,
+	)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			return nil, ErrNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &role, nil
+}
