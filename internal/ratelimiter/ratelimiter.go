a6ac049646bfb65d343d6428cc20d9670de22c9d
Mohamed Ibranim <mosuliman92@gmail.com>
2025-10-22 07:45:26 +0300

    add ratelimiter package to limit requests from specific ip

---
+++ internal/ratelimiter/ratelimiter.go
@@ -0,0 +1,14 @@
+// Package ratelimiter contains code that handle ratelimiter to requests different algorithms.
+package ratelimiter
+
+import "time"
+
+type Limiter interface {
+	Allow(ip string) (bool, time.Duration)
+}
+
+type Config struct {
+	RequestsPerTimeFrame int
+	TimeFrame            time.Duration
+	Enabled              bool
+}
